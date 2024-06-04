eb94d18c7e90ba2209027641080fb927c30a28df
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-06-04 13:51:14 +0300

    Finish task

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 40, 'M', 'black', 'sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'white', 'melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 20, 'F', 'black', 'sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 50, 'M', 'white', 'choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 60, 'F', 'black', 'melanchonic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 30, 'M', 'white', 'sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 20, 'F', 'white', 'phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, 'M', 'white', 'sanguine', 1);
